6ff804aaf5d69b3143174c2d0e1f03976225cc81
ginesgz <65681475+ginesgz@users.noreply.github.com>
2024-11-22 11:59:10 -0300

    Update streamlit_app.py
    Added API call for watermelon for each fruit, just for testing purposes

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-    
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     
     # st.write(ingredients_string)
     
@@ -46,7 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
         
-# New section to display smoothie nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
